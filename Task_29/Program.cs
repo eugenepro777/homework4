916327575ef3a29a369cbd27a142aea281d888ef
Eugene_Pro <proskuryakov.e@list.ru>
2022-11-24 22:54:36 +0300

    update description

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -15,7 +15,14 @@ void PrintArray(int[] arr)
 {
     for(int i = 0; i < arr.Length; i++)
     {
-        Console.Write($"{arr[i]}, ");
+        if(i == arr.Length - 1)
+        {
+            Console.Write($"{arr[i]}");
+        }
+        else
+        {
+            Console.Write($"{arr[i]}, ");
+        }
     }
 }
 
